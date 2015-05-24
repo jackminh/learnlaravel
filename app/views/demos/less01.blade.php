a9b726618fdb79966331c3818700a73a563ae4d9
jackminh <jackminh@outlook.com>
2015-05-24 11:30:47 +0800

    add all files

---
+++ app/views/demos/less01.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang='zh-CN'>
+<head>
+<link rel="stylesheet/less" type="text/css" href="{{asset('css/demo/style.less')}}" />
+<!-- set options before less.js script -->
+<script>
+  less = {
+    env: "development",
+    async: false,
+    fileAsync: false,
+    poll: 1000,
+    functions: {},
+    dumpLineNumbers: "comments",
+    relativeUrls: false,
+    rootpath: ":/a.com/"
+  };
+</script>
+<script src="{{asset('js/less.js-master/dist/less.js')}}" type="text/javascript"></script>
+</head>
+<body>
+
+
+aaaa
+
+</body>
+</html>
